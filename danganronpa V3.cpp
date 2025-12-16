bcaf3757e4afa25ed955f3a4b15a31acc2163c53
LSTR-512 <136193132+Elster-5120@users.noreply.github.com>
2025-12-16 13:54:42 +0800

    Update danganronpa V3.cpp

--- danganronpa V3.cpp
+++ danganronpa V3.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std; 
 
-// === Global arrays === 
 string names[16] = {
     "Rantaro Amami", "Kaede Akamatsu", "Ryoma Hoshi", "Kirumi Tojo",
     "Angie Yonaga", "Tenko Chabashira", "Korekiyo Shinguji", "Miu Iruma",
@@ -18,25 +17,24 @@ string talents[16] = {
 }; 
 
 string fates[16] = {
-    "CAUSE OF DEATH: Killed by Tsumugi Shirogane with a shot put ball in the library.", // Rantaro
-    "CAUSE OF DEATH: Wrongfully executed for Rantaro's murder after Monokuma framed her.", // Kaede
-    "CAUSE OF DEATH: Murdered by Kirumi Tojo, drowned in the library aquarium.", // Ryoma
-    "CAUSE OF DEATH: Dragged up a spiked rope and fell to her death.", // Kirumi
-    "CAUSE OF DEATH: Murdered by Korekiyo Shinguji with a saw during a ritual.", // Angie
-    "CAUSE OF DEATH: Murdered by Korekiyo Shinguji during the Séance ritual.", // Tenko
-    "CAUSE OF DEATH: Executed after being revealed as the mastermind.", // Korekiyo
-    "CAUSE OF DEATH: Strangled by Gonta Gokuhara in the virtual world.", // Miu
-    "CAUSE OF DEATH: Executed after being tricked into killing Miu in the virtual world.", // Gonta
-    "CAUSE OF DEATH: Poisoned himself and crushed by a hydraulic press in a plan with Kaito.", // Kokichi
-    "CAUSE OF DEATH: Died from lethal injection alongside Kokichi.", // Kaito
-    "CAUSE OF DEATH: Sacrificed himself in the final battle.", // K1-BO
-    "CAUSE OF SURVIVAL: Survived the final killing game after confronting the mastermind.", // Shuichi
-    "CAUSE OF SURVIVAL: Survived the final killing game alongside Shuichi and Maki.", // Himiko
-    "CAUSE OF SURVIVAL: Survived the final killing game alongside Shuichi and Himiko.", // Maki
-    "CAUSE OF DEATH: Executed by a hydraulic press as the mastermind." // Tsumugi
+    "CAUSE OF DEATH: Killed by Tsumugi Shirogane with a shot put ball in the library.",
+    "CAUSE OF DEATH: Wrongfully executed for Rantaro's murder after Monokuma framed her.",
+    "CAUSE OF DEATH: Murdered by Kirumi Tojo, drowned in the library aquarium.",
+    "CAUSE OF DEATH: Dragged up a spiked rope and fell to her death.",
+    "CAUSE OF DEATH: Murdered by Korekiyo Shinguji with a saw during a ritual.",
+    "CAUSE OF DEATH: Murdered by Korekiyo Shinguji during the Séance ritual.",
+    "CAUSE OF DEATH: Executed after being revealed as the mastermind.",
+    "CAUSE OF DEATH: Strangled by Gonta Gokuhara in the virtual world.",
+    "CAUSE OF DEATH: Executed after being tricked into killing Miu in the virtual world.",
+    "CAUSE OF DEATH: Poisoned himself and crushed by a hydraulic press in a plan with Kaito.",
+    "CAUSE OF DEATH: Died from lethal injection alongside Kokichi.",
+    "CAUSE OF DEATH: Sacrificed himself in the final battle.",
+    "CAUSE OF SURVIVAL: Survived the final killing game after confronting the mastermind.",
+    "CAUSE OF SURVIVAL: Survived the final killing game alongside Shuichi and Maki.",
+    "CAUSE OF SURVIVAL: Survived the final killing game alongside Shuichi and Himiko.",
+    "CAUSE OF DEATH: Executed by a hydraulic press as the mastermind."
 };
 
-// === Global Variables === 
 string choice; 
 int value; 
 
@@ -46,7 +44,7 @@ void showChar(int index) {
     cout << "\n" << fates[index]; 
 }
 
-void runGame() {
+bool runGame() {
     cout << "\nSelect a character to know their talent and cause of death/survival.";
     cout << "\nBEWARE OF SPOILERS!!";
 
@@ -55,19 +53,21 @@ void runGame() {
     }
     cout << "\n17. Cancel";
 
-    cout << "\n\nEnter your selection (1-16): ";
+    cout << "\n\nEnter your selection (1-17): ";
     cin >> value;
 
     if (value >= 1 && value <= 16) {
         showChar(value-1);
     } else if (value == 17) {
-        cout << "\nPlease press the 'Enter' key to quit!";
+        cout << "\nExiting game...";
+        return false;
     } else {
-        cout << "\nInvalid selection. Please enter a number between 1 and 16.";
+        cout << "\nInvalid selection. Please enter a number between 1 and 17.";
     }
+    return true;
 }
 
-void menu() {
+bool menu() {
     cout << "\n\n=== DANGANRONPA V3: KILLING HARMONY ===";
     cout << "\nStart Game? yes or no?";
     cout << "\nPlease enter yes and no in lowercase!";
@@ -76,18 +76,18 @@ void menu() {
     cin >> choice;
 
     if (choice == "no") {
-        cout << "\nAre you sure? Your progress might not be saved.";
-        cout << "\nPress the Enter key to quit.";
+        cout << "\nExiting program...";
+        return false;
     } else if (choice == "yes") {
-        runGame();
+        return runGame();
     } else {
         cout << "\nInvalid initial choice. Please enter 'yes' or 'no'!";
+        return true;
     }
 }
 
 int main() {
-    while(true) {
-        menu(); 
+    while(menu()) {
     }
     return 0; 
 }
