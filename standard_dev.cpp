40e3f21b27c7b96d05f8842479f6dacd18343859
LSTR-512 <136193132+Elster-5120@users.noreply.github.com>
2026-01-10 18:50:35 +0800

    Create standard_dev.cpp

---
+++ standard_dev.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+
+double x1, x2, x3, x4, x5;
+double mean, std_dev; 
+
+double comp_mean(){
+    mean = (x1 + x2 + x3 + x4 + x5) / 5.0; 
+    return mean; 
+}
+
+double comp_std(){
+    std_dev = sqrt( 
+        ((x1 - mean) * (x1 - mean) + 
+        (x2 - mean) * (x2 - mean) + 
+        (x3 - mean) * (x3 - mean) + 
+        (x4 - mean) * (x4 - mean) + 
+        (x5 - mean) * (x5 - mean)) / 5.0 );
+    return std_dev; 
+}
+
+void menu(){
+    cout << "Enter the value for x1: \n> "; 
+    cin >> x1; 
+    cout << "Enter the value for x2: \n> "; 
+    cin >> x2; 
+    cout << "Enter the value for x3: \n> "; 
+    cin >> x3; 
+    cout << "Enter the value for x4: \n> "; 
+    cin >> x4; 
+    cout << "Enter the value for x5: \n> "; 
+    cin >> x5; 
+
+    comp_mean(); 
+    comp_std(); 
+    cout << "The mean is: " << mean << "." << endl; 
+    cout << "The standard deviation is: " << std_dev << "." << endl; 
+}
+
+int main(){
+    system("cls"); 
+    menu(); 
+    system("pause"); 
+    return 0; 
+}
